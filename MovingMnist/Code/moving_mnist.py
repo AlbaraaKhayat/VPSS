f4ab3fbcca156787ace88ffbd1208e95a70b8ca1
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-11-09 00:40:23 +0300

    Update moving_mnist.py

--- MovingMnist/Code/moving_mnist.py
+++ MovingMnist/Code/moving_mnist.py
@@ -6,44 +6,42 @@ import scipy.misc as misc
 import sys
 
 data_array = np.zeros((100, 30, 64, 64, 1),dtype=np.float32)
-class MyThread(threading.Thread):
-    def __init__(self, train, seq_len = 30, num_digits = 1, image_size = 64, data_slice = -1):
-        super(MyThread, self).__init__()  
 
+class MyThread(threading.Thread):
+    def __init__(self, ):
+
+        super(MyThread, self).__init__()  
         self.data = Xall
-        self.N = self.data.shape[0]
-        self.seq_len = seq_len
+        self.N = length of possible start
+        self.seq_len = dms
         self.num_digits = num_digits
         self.image_size = image_size
         self.digit_size = 28
         self.channels = 1 
         self.data_slice = data_slice
 
+
     def run(self):
         image_size = self.image_size
         digit_size = self.digit_size
         x = np.zeros((self.seq_len,image_size, image_size, self.channels),dtype=np.float32)
-        for n in range(self.num_digits):
-            idx = np.random.randint(self.N)
-            digit = misc.imresize(self.data[idx], [self.digit_size, self.digit_size])
-
+        idx = np.random.randint(self.N)
+        digit = self.data[idx]
         data_array[self.data_slice, 0:self.seq_len] = x
 
 class mnist(object):
-    def __init__(self, train, batch_size = 100, seq_len = 30, num_digits = 1, image_size = 64):
+    def __init__(self,train, batch_size = 1, seq_len = 0, image_size = 160):
         self.train = train
         self.seq_len = seq_len
-        self.num_digits = num_digits
         self.image_size = image_size
         self.batch_size = batch_size
-        self.digit_size = 28
         self.channels = 1 
 
-    def getbatch(self):
+    def getbatch(self,sources=None):
         # data_array = np.zeros((100, 15, 64, 64, 1),dtype=np.float32)
         th_pool = []
         for i in range(self.batch_size):
-            th = MyThread(self.train, self.seq_len, self.num_digits, self.image_size, i)
+            th = MyThread(self.split, self.seq_len, self.image_size, i)
             th.start()
             th_pool.append(th)
         for i in range(self.batch_size):
